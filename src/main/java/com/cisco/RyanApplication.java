b7d7f9b9dc41bf66921cf7fa4a017d7962b13caf
Steve Prentice <prentice@cisco.com>
2015-03-25 12:53:03 -0700

    Initial structure.

---
+++ src/main/java/com/cisco/RyanApplication.java
@@ -0,0 +1,11 @@
+package com.cisco;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class RyanApplication extends Application<RyanConfig> {
+    @Override
+    public void run(RyanConfig ryanConfig, Environment environment) throws Exception {
+        
+    }
+}
---
+++ src/main/java/com/cisco/RyanConfig.java
@@ -0,0 +1,6 @@
+package com.cisco;
+
+import io.dropwizard.Configuration;
+
+public class RyanConfig extends Configuration{
+}
